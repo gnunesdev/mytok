b471e19094d4cfa1d38347c8481c73937abd8870
gnunesdev <gnunesinf@gmail.com>
2023-04-06 23:15:06 -0300

    feat: sets up tailwindcss

---
+++ apps/front/src/app/index.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import './../styles/global.css'
+
+export function App() {
+  return (
+    <h1 className="text-cyan-900">Hello</h1>
+  );
+}
+
+export default App;
